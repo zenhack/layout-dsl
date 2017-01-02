e3f67968535657291c5ee258f9f98509d39797bf
Ian Denhardt <ian@zenhack.net>
2017-01-01 20:33:45 -0500

    Main: delve a little further into the lib
    Our toy wrapper now tries to validate.

--- prototype/Main.hs
+++ prototype/Main.hs
@@ -1,9 +1,15 @@
 module Main where
 
+import Layout.Ast (File(..))
 import Layout.Parser
+import Layout.Validate (buildSyms)
 import Text.ParserCombinators.Parsec (runParser)
 
 main :: IO ()
 main = do
     contents <- getContents
-    print (runParser pFile () "" contents)
+    case runParser pFile () "<stdin>" contents of
+        Left err -> putStrLn $ "Parse error: " ++ show err
+        Right (File decls)-> case buildSyms decls of
+            Left err -> putStrLn $ "validation error: " ++ show err
+            ok@(Right _) -> print ok
