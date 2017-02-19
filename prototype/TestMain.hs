e1037c5a46f0c9f1993bef3b0750c860c8a43a33
Ian Denhardt <ian@zenhack.net>
2017-02-19 16:33:09 -0500

    Remove orphaned SubSurface module.
    It has now been fully replaced by the parametrized Ast.

--- prototype/TestMain.hs
+++ prototype/TestMain.hs
@@ -8,7 +8,6 @@ import Tests.IR.RangeMap (rangeMapTests)
 -- Just so we're building these at all; will probably remove once we have
 -- actual tests.
 import qualified Layout.IR.MachineOps
-import qualified Layout.IR.SubSurface
 import qualified Layout.Target.C
 import qualified Layout.Target.Haskell
 
