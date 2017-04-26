66782d1e7bda4bcadfab86866c6e4b9bfbf78f28
Ian Denhardt <ian@zenhack.net>
2017-04-25 23:15:04 -0400

    Start work converting RangeMaps to MachineOps.
    Untested.

--- prototype/Language/LDSL/IR/MachineOps.hs
+++ prototype/Language/LDSL/IR/MachineOps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 {-|
 
 Intermediate form that works at the level of close-to-the-machine operations:
@@ -8,6 +9,7 @@ last leg before generating getters/setters for a target language.
 -}
 module Language.LDSL.IR.MachineOps where
 
+import Language.LDSL.IR.RangeMap(RangeMap(..), Index(..))
 import Data.Word
 
 data ByteN = ByteN Int
@@ -28,3 +30,48 @@ data BinOp
 
 data GetStmt = GetStmt (Expr ByteN)
 data SetStmt = SetStmt Word64 (Expr Arg)
+
+data Field = Field
+    { fieldGetter :: GetStmt
+    , fieldSetter :: [SetStmt]
+    }
+
+getFieldOps :: [(RangeMap Index, Int)] -> Field
+getFieldOps rm = Field
+    { fieldGetter = getFieldRange rm
+    , fieldSetter = setFieldRange (map fst rm)
+    }
+
+getFieldRange :: [(RangeMap Index, Int)] -> GetStmt
+getFieldRange [] = GetStmt $ Const 0 -- TODO: not sure this should ever occur
+getFieldRange ranges = GetStmt $
+    foldl
+        (BinOp BitOr)
+        (Const 0)
+        $ map (uncurry getByteRange) ranges
+
+getByteRange :: RangeMap Index -> Int -> Expr ByteN
+getByteRange RangeMap{..} sz =
+    let
+        Index index = rangeInfo
+        upcast      = Cast sz $ Src $ ByteN index
+        shifted     = BinOp ShiftR upcast (Const $ fromIntegral rangeLayoutOff)
+        mask        = fromIntegral $ 2 ^ rangeLen - 1
+        truncated   = BinOp BitAnd (Const mask) shifted
+        final       = BinOp ShiftL truncated (Const $ fromIntegral rangeTypeOff)
+    in
+        final
+
+
+setFieldRange :: [RangeMap Index] -> [SetStmt]
+setFieldRange = map setByteRange
+
+setByteRange :: RangeMap Index -> SetStmt
+setByteRange RangeMap{..} =
+    let
+        Index index = rangeInfo
+        mask        = fromIntegral $ 2 ^ rangeLen - 1
+        shifted     = BinOp ShiftR (Src Arg) (Const $ fromIntegral rangeLayoutOff)
+        result      = BinOp BitAnd (Const mask) shifted
+    in
+        SetStmt (fromIntegral index) result
